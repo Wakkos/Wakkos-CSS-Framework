e74eeb41b8c769e8cf7041d5bd44ddc8d8f7df8b
Wakkos <wakkos@gmail.com>
2017-12-24 16:37:41 +0100

    Set gulp compile and watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,13 @@
-/**
-* Configuración del gulpFile
-**/
-config = {
-  autoprefixer : true, //Prefijos de navegadores para CSS: compatibilidad con browsers
-  minify : true, // Minificado de CSS
-  mergeMediaQueries: true, // Unimos el interior de las mediaQueries con las misma condición
-  paths : {
-    cssPath : 'css',
-    sassPath : 'scss',
-    cssName: 'style.css',
-    sassName: 'style.scss'
-  }
-}
-// Variables que nos facilitan crear las tareas gulp
-var sassDir = config.paths.sassPath;
-var sassFile = sassDir + '/' + config.paths.sassName;
-var cssDir = config.paths.cssPath;
-var cssFile = cssDir + '/' + config.paths.cssName;
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
-/**
-* Includes: Añadimos las librerías necesarios para compilar el Sass con nuestra configuración
-**/
-var gulp = require('gulp'); // Gulp
-var gulpif = require('gulp-if'); // Condiciones para gulp
-var sass = require('gulp-sass'); // Compilador Sass
-var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-minify-css');
-var mmq = require('gulp-merge-media-queries');
+gulp.task('sass', function(){
+    return gulp.src('scss/style.scss')
+      .pipe(sass()) // Using gulp-sass
+      .pipe(gulp.dest('css'))
+  });
 
-/**
-* Config de autoprefixer.
-**/
-var AUTOPREFIXER_BROWSERS = ['ie >= 9','ie_mob >= 10','ff >= 30','chrome >= 34','safari >= 7','opera >= 23','ios >= 7','android >= 4.4','bb >= 10']; // Compatibilidad css
-
-gulp.task('sass', function() {
-    return gulp.src([sassFile])
-        .pipe(sass().on('error', function(err) {
-                console.log(err.toString());
-                this.emit('end');
-            }
-        ))
-        .pipe(gulpif(config.autoprefixer, autoprefixer({browsers: AUTOPREFIXER_BROWSERS})))
-        .pipe(gulpif(config.mergeMediaQueries, mmq()))
-        .pipe(gulpif(config.minify, minify()))
-        .pipe(gulp.dest(cssDir));
-});
-
-
-// Ejecuta funciones cuando el archivo cambia
-gulp.task('watch', function() {
-    gulp.watch([sassDir + '/*/*.scss',sassFile],['sass']);
-});
-
-// Default Task
-gulp.task('default', ['watch']);
+  gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', ['sass']);
+    // Other watchers
+  })
